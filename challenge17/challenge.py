a31c76922f63556c769771e23448a3bbbacf6895
rmartinez110 <robert.martinez@tlgcohort.com>
2023-10-30 20:53:29 +0000

    object methods complete

---
+++ challenge17/challenge.py
@@ -0,0 +1,9 @@
+#! usr/bin/python3
+
+def main():
+    userName = input("What is your name?\n>");
+    weekDay = input("What is your age\n>");
+
+    print("Hello, ", userName,"! Happy ", weekDay)
+
+main()
---
+++ challenge17/tartswith("eth")
@@ -0,0 +1,4 @@
+Help on built-in function lower:
+
+lloowweerr() method of builtins.str instance
+    Return a copy of the string converted to lowercase.
