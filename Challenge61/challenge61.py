43af0a9a8a08f1be98851c3eeab49b6e8af9b7ea
rmartinez110 <robert.martinez@tlgcohort.com>
2023-11-06 17:47:50 +0000

    json and yaml

---
+++ Challenge61/challenge61.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+
+def main():
+    for i in range(101, -1, -1):
+            print(f"{i} bottles of beer on the wall!")
+            print(f"{i} bottles of beer on the wall! {i} bottles of beer! You take one down, pass it around!")
+
+if __name__ == "__main__":
+    main()
